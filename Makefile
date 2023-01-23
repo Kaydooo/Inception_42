4d678da754f99fd0d4347ce262a2d41c6c5a4317
Moatasem <m3t9mm@gmail.com>
2023-01-24 01:59:23 +0400

    :)

---
+++ Makefile
@@ -0,0 +1,11 @@
+all: wp-data
+	docker compose up
+	
+wp-data:
+	mkdir -p "/opt/data/wp-data"
+	chmod 777 "/opt/data/"
+	
+	
+re: wp-data
+	docker compose down
+	docker compose up --build
---
+++ wordpress/conf/setup.sh
@@ -0,0 +1,29 @@
+#!bin/sh
+
+sed -i 's/listen =.*/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+mkdir -p /run/php
+
+service mysql start
+
+if ! command -v wp &> /dev/null
+then
+  echo "WP-CLI not found, downloading and setting up..."
+  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  chmod +x wp-cli.phar
+  mv wp-cli.phar /usr/local/bin/wp
+fi
+
+if [ ! -d "/var/www/html/wp-content" ]; then
+  mysql -uroot  -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'kaydoo'@'localhost' IDENTIFIED BY 'lujwll'; 	   FLUSH PRIVILEGES;"
+  echo "wp-content directory not found, installing WordPress..."
+  wp --allow-root core download
+  wp --allow-root config create --dbname=wordpress --dbuser=kaydoo --dbpass=lujwll
+  #wp --allow-root db create
+  wp --allow-root core install --url=localhost --title=KinG --admin_user=kaydooo --admin_password=lujwll --admin_email=m3t9mm@gmail.com
+
+  echo "WordPress has been successfully installed."
+else
+  echo "wp-content directory found, WordPress already installed."
+fi
+
+/usr/sbin/php-fpm7.3 --nodaemonize
