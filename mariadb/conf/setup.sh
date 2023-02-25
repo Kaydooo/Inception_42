2188869dd09ce066a2d42e5414376b128fd2c091
Moatasem <m3t9mm@gmail.com>
2023-02-25 17:27:56 +0400

    remove infinite sleep and replace it with mysqld --console to run in foreground

--- mariadb/conf/setup.sh
+++ mariadb/conf/setup.sh
@@ -2,19 +2,19 @@
 
 sed -i 's/bind-address .*=.*/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i 's/.port .*=.*/port = 3306/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
 service mysql start
 
 #mysqld --console 
 
 sleep 3
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-  mysql -uroot  -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON *.* TO 'kaydoo'@'%' IDENTIFIED BY 'lujwll'; 	   FLUSH PRIVILEGES;"
+  mysql -uroot  -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'; 	   FLUSH PRIVILEGES;"
   echo "wordpress database created "
 
 fi
 
+service mysql stop
 
-sed -i 's/bind-address .*=.*/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i 's/.port .*=.*/port = 3306/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+mysqld --console
 
-sleep 1000000
